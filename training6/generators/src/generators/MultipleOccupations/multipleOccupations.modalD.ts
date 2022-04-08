e7b12094ea6d4af625f676a3d62db144eae76b87
Alin Seras <alinseras@192-168-0-100.rdsnet.ro>
2022-04-08 11:47:55 +0300

    remove hide buttons from training 6

--- training6/generators/src/generators/MultipleOccupations/multipleOccupations.modalD.ts
+++ training6/generators/src/generators/MultipleOccupations/multipleOccupations.modalD.ts
@@ -3,13 +3,14 @@ import {
     oneOccupationIncomeDetailsDD,
 } from "./multipleOccupations.dataD";
 import {HideButtonsCD} from "@focuson/forms/dist/src/buttons/hideButtonsCD";
+import {LayoutCd} from "@focuson/forms";
 
 export const editOccupationIncomeSummaryModalPD: TrainingModalPage = {
     name: 'OccupationIncomeModal',
     pageType: 'ModalPage',
     modes: [ 'view','create','edit' ],
     display: { target: '~/tempForOccupationEdit', dataDD: oneOccupationIncomeDetailsDD },
-    layout: { component: HideButtonsCD, displayParams: { hide: [ 'additionalInfoFirst', 'additionalInfoSecond', 'otherSourcesOfIncome', 'list' ] } },
+    layout: { component: LayoutCd, displayParams: { details: '[[30]]' } },
     buttons: {
         cancel: { control: 'ModalCancelButton' },
         commit: { control: 'ModalCommitButton' }
